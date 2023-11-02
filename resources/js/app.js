0467522267754139da69aa734a59aa6dd678cc9a
Firas <firasmumtaz12@gmail.com>
2023-11-02 23:32:42 +0530

    added breeze api

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import "./bootstrap";
-import "../css/app.css";
-
-import { createApp } from "vue";
-import App from "./Components/App.vue"; // Main App component
-import router from "./router.js"; // Vue Router
-
-createApp(App).use(router).mount("#app");

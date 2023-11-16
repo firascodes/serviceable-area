587508fee655687fccf57094a61eb58f4014731c
Firas <firasmumtaz12@gmail.com>
2023-11-16 21:28:21 +0530

    added buttons & functiions

--- frontend/src/components/MapComponent.vue
+++ frontend/src/components/MapComponent.vue
@@ -1,86 +1,90 @@
 <template>
   <div class="w-full h-full">
     <GMapMap :center="center" :zoom="10" class="w-full h-full" ref="gmap">
-      <GMapMarker v-for="(marker, index) in markers" :key="index"
-        :position="marker" :icon='{
-          url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/640px-Map_marker.svg.png",
-          scaledSize: { width: 35, height: 50 },
+      <GMapMarker
+        v-for="(marker, index) in markers"
+        :key="index"
+        :position="marker"
+        :icon="{
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/640px-Map_marker.svg.png',
+          scaledSize: { width: 35, height: 50 }
           // labelOrigin: {x: 16, y: -10}
-        }' />
-      <GMapPolygon v-if="polygon && polygon.length" :paths="polygon"
-        :options="options" :editable="editable" />
+        }"
+      />
+      <GMapPolygon
+        v-if="polygon && polygon.length"
+        :paths="polygon"
+        :options="options"
+        :editable="editable"
+      />
     </GMapMap>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'MapComponent',
   props: ['markers', 'polygon', 'drawing', 'editable'],
   data() {
     return {
-      center: { lat: 28.6139, lng: 77.2090 },
+      center: { lat: 28.6139, lng: 77.209 },
       curentPolygon: null,
       drawingManager: null,
       options: {
-        fillColor: "#93C5FD",
+        fillColor: '#93C5FD',
         fillOpacity: 0.6,
         strokeWeight: 2,
         clickable: false,
-        zIndex: 1,
-      },
+        zIndex: 1
+      }
     }
   },
 
   methods: {
     async drawPolygon() {
-      const map = await this.$refs.gmap.$mapObject;
-      const google = await this.$gmapApiPromiseLazy();
+      const map = await this.$refs.gmap.$mapObject
+      const google = await this.$gmapApiPromiseLazy()
       if (this.drawingManager) {
-        this.drawingManager.setMap(null);
+        this.drawingManager.setMap(null)
       }
       this.drawingManager = new google.maps.drawing.DrawingManager({
         drawingMode: google.maps.drawing.OverlayType.POLYGON,
         drawingControl: true,
         drawingControlOptions: {
           position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            google.maps.drawing.OverlayType.POLYGON
-          ]
-        },
-      });
-      this.drawingManager.setMap(map);
+          drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+        }
+      })
+      this.drawingManager.setMap(map)
       google.maps.event.addListener(this.drawingManager, 'overlaycomplete', (event) => {
         if (event.type == google.maps.drawing.OverlayType.POLYGON) {
           if (this.currentPolygon) {
-            this.currentPolygon.setMap(null);
+            this.currentPolygon.setMap(null)
           }
 
           // Keep a reference to the current polygon
-          this.currentPolygon = event.overlay;
+          this.currentPolygon = event.overlay
 
-          this.$emit('polygon-drawn', event.overlay);
-          this.drawingManager.setMap(null);
+          this.$emit('polygon-drawn', event.overlay)
+          this.drawingManager.setMap(null)
         }
         // const polygon = event.overlay.getPath().getArray().map(coord => ({
         // lat: coord.lat(),
         //   lng: coord.lng()
         // }));
-      });
+      })
     }
   },
   watch: {
-    'drawing': function (newVal, oldVal) {
+    drawing: function (newVal, oldVal) {
       if (newVal !== oldVal && newVal === true) {
-        this.drawPolygon();
+        this.drawPolygon()
       }
     }
   },
   mounted() {
-    this.drawPolygon();
+    this.drawPolygon()
   }
-
 }
 </script>
 
--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,113 +1,170 @@
 <template>
-    <div>
-        <h1 class="text-2xl font-bold">Admin</h1>
-        <div class="flex gap-2 ">
-            <button @click="createPolygon">Create Polygon </button>
-            <button @click="savePolygon" v-if="polygon"> Save Polygon</button>
-            <button @click="togglePolygonEdit" v-if="polygon && !editing">Edit
-                Polygon</button>
-            <button @click="togglePolygonEdit"
-                v-if="polygon && editing">Done</button>
-        </div>
-        <div class="w-full h-[500px]">
-            <MapComponent :markers="markers" :polygon="polygon" :drawing="drawing"
-                :editable="editable" @map-clicked="addMarker"
-                @polygon-updated="updatePolygon" @polygon-drawn="polygonDrawn" />
-        </div>
-        <div v-if="polygon"
-            class="mt-4 text-blue-500 text-xs px-3 py-2 flex flex-col">
-            <h2 class="text-xl font-bold">Polygon Coordinates:</h2>
-            <ul class="text-md gap-2">
-                <li v-for="(coord, index) in polygon" :key="index">
-                    Latitude: {{ coord.lat }}, Longitude: {{ coord.lng }}
-                </li>
-            </ul>
-        </div>
+  <div>
+    <h1 class="text-2xl font-bold">Admin</h1>
+    <div class="flex gap-2 m-2">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="createPolygon"
+      >
+        Create Polygon
+      </button>
+      <button
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        @click="savePolygon"
+        v-if="polygon"
+      >
+        Save Polygon
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="togglePolygonEdit"
+        v-if="polygon && !editing"
+      >
+        Edit Polygon
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="togglePolygonEdit"
+        v-if="polygon && editing"
+      >
+        Done
+      </button>
+      <button
+        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        @click="erasePolygon"
+        v-if="polygon"
+      >
+        Erase
+      </button>
     </div>
+    <div class="w-full h-[500px]" v-if="showMap">
+      <MapComponent
+        :markers="markers"
+        :polygon="polygon"
+        :drawing="drawing"
+        :editable="editable"
+        @map-clicked="addMarker"
+        @polygon-updated="updatePolygon"
+        @polygon-drawn="polygonDrawn"
+      />
+    </div>
+    <div v-if="polygon" class="mt-4 text-blue-500 text-xs px-3 py-2 flex flex-col">
+      <h2 class="text-xl font-bold">Polygon Coordinates:</h2>
+      <ul class="text-md gap-2">
+        <li v-for="(coord, index) in polygon" :key="index">
+          Latitude: {{ coord.lat }}, Longitude: {{ coord.lng }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-import axios from 'axios';
-import MapComponent from '../components/MapComponent.vue';
+import axios from 'axios'
+import MapComponent from '../components/MapComponent.vue'
 
 export default {
-    components: {
-        MapComponent
-    },
-    data() {
-        return {
-            markers: [],
-            polygon: null,
-            drawing: false,  //
-            editable: false,
-            editing: false,  // Add this line
-        }
-    },
-    methods: {
-        addMarker(event) {
-            this.markers.push({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-            console.log("addMarker");
-        },
-        async polygonDrawn(polygon) {
-            const google = await this.$gmapApiPromiseLazy();
-            console.log(this.polygon.getPath().getArray());
-            this.polygon = polygon.getPath().getArray().map(coord => ({
-                lat: coord.lat(),
-                lng: coord.lng()
-            }));
-            this.drawing = false;  // Set drawing to false
-
-            let vueInstance = this;
-
-            // Add event listeners for the set_at and insert_at events
-            google.maps.event.addListener(polygon, 'set_at', function () {
-                console.log('set_at event triggered');
-                vueInstance.updatePolygon(polygon);
-            });
-            google.maps.event.addListener(polygon, 'insert_at', function () {
-                console.log('insert_at event triggered');
-                vueInstance.updatePolygon(polygon);
-            });;
-        },
-        createPolygon() {
-            this.drawing = true;  // Set drawing to true
-
-            // this.polygon = null;
-            // this.markers = [];
-        },
-        updatePolygon(polygon) {
-            this.polygon = polygon.getPath().getArray().map(coord => ({
-                lat: coord.lat(),
-                lng: coord.lng()
-            }));
-            console.log(this.polygon);
-
-        },
-        togglePolygonEdit() {
-            this.editable = !this.editable;
-            this.editing = !this.editing;
-        },
-        async savePolygon() {
-            try {
-                // Convert the polygon vertices to the format expected by the API
-                const boundary_coordinates = this.polygon.map(marker => ({
-                    latitude: marker.lat,
-                    longitude: marker.lng
-                }));
-
-                console.log(boundary_coordinates);
-
-                const response = await axios.post('http://127.0.0.1:8000/api/save-serviceable-area', { boundary_coordinates });
-                // Handle the response
-                if (response.data.message) {
-                    alert(response.data.message);  // Show a success message
-                    // Clear the polygon data
-                    this.polygon = null;
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
+  components: {
+    MapComponent
+  },
+  data() {
+    return {
+      markers: [],
+      polygon: null,
+      drawing: false, //
+      editable: false,
+      editing: false,
+      showMap: true // Add this line
     }
+  },
+  methods: {
+    addMarker(event) {
+      this.markers.push({ lat: event.latLng.lat(), lng: event.latLng.lng() })
+      console.log('addMarker')
+    },
+    async polygonDrawn(polygon) {
+      if (polygon) {
+        const google = await this.$gmapApiPromiseLazy()
+        console.log(polygon.getPath().getArray())
+        this.polygon = polygon
+          .getPath()
+          .getArray()
+          .map((coord) => ({
+            lat: coord.lat(),
+            lng: coord.lng()
+          }))
+        this.drawing = false // Set drawing to false
+
+        let vueInstance = this
+
+        // Add event listeners for the set_at and insert_at events
+        google.maps.event.addListener(polygon, 'set_at', function () {
+          console.log('set_at event triggered')
+          vueInstance.updatePolygon(polygon)
+        })
+        google.maps.event.addListener(polygon, 'insert_at', function () {
+          console.log('insert_at event triggered')
+          vueInstance.updatePolygon(polygon)
+        })
+      }
+    },
+
+    createPolygon() {
+      this.drawing = true // Set drawing to true
+
+      // this.polygon = null;
+      // this.markers = [];
+    },
+    erasePolygon() {
+      // Briefly remove the MapComponent from the DOM
+      this.showMap = false
+
+      // Clear the polygon data
+      this.polygon = null
+      this.markers = []
+
+      // Bring the MapComponent back to the DOM
+      this.$nextTick(() => {
+        this.showMap = true
+      })
+    },
+    updatePolygon(polygon) {
+      this.polygon = polygon
+        .getPath()
+        .getArray()
+        .map((coord) => ({
+          lat: coord.lat(),
+          lng: coord.lng()
+        }))
+      console.log(this.polygon)
+    },
+    togglePolygonEdit() {
+      this.editable = !this.editable
+      this.editing = !this.editing
+    },
+    async savePolygon() {
+      try {
+        // Convert the polygon vertices to the format expected by the API
+        const boundary_coordinates = this.polygon.map((marker) => ({
+          latitude: marker.lat,
+          longitude: marker.lng
+        }))
+
+        console.log(boundary_coordinates)
+
+        const response = await axios.post('http://127.0.0.1:8000/api/save-serviceable-area', {
+          boundary_coordinates
+        })
+        // Handle the response
+        if (response.data.message) {
+          alert(response.data.message) // Show a success message
+          // Clear the polygon data
+          this.polygon = null
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
 }
 </script>
